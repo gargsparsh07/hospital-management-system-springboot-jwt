4818deb232d2ca33f66e92a10c75a4edb1814f5f
Sparsh Garg <sparshgarg.pks@gmail.com>
2026-04-20 14:53:44 +0530

    [Sparsh]: Feat: add user role and patient entity classes

---
+++ src/main/java/com/example/hms/entity/Patient.java
@@ -0,0 +1,32 @@
+package com.example.hms.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "patients")
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+
+    private String gender;
+
+    private String disease;
+
+    private String address;
+
+    private String phoneNumber;
+}
---
+++ src/main/java/com/example/hms/entity/User.java
@@ -0,0 +1,30 @@
+package com.example.hms.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
